a5b79a6f6c496da04ae7ba66930861da420bfe30
mariolopez15 <mario.lopez@udc.es>
2022-10-20 16:59:45 +0200

    implementado con interrupciones

--- main.c
+++ main.c
@@ -44,48 +44,39 @@ void led_red_toggle(void)
 }
 
 void button_init(){
-    /*
-    SIM->COPC = 0;
-    SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK; //habilitamos puerto C
-    PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1);
-    GPIOC->PDDR &= ~(1 << 3); //lo establecemos como entrada (0)
-    */
 
     SIM->COPC = 0;
     SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK; //habilitamos puerto C
     //establecemos la interrupcion on fallin edge (1010)
-    PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_IRQC(6) ;
+    PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_IRQC(0xA) ;
     GPIOC->PDDR &= ~(1 << 3); //lo establecemos como entrada (0)
-    NVIC_EnableIRQ(47); //habilitamos la interrupcion
-    NVIC_SetPriority(47, 0);
+
+    NVIC_SetPriority(31, 0); //prioridad max
+    NVIC_EnableIRQ(31); //habilitamos la interrupcion
+
+
 
 }
 
-void PORTDIntHandler(){
+void PORTDIntHandler(void){
 
     PORTC->PCR[3] |=PORT_PCR_ISF(1); //limpiamos la interrupcion
+    led_green_toggle();
+    led_red_toggle();
 }
 
-//quitar el check
-int sw1_check(){
-    return(!(GPIOC->PDIR & (1<<3)));
-}
 
 int main(void)
 {
     button_init();
-  led_green_init();
-
-  while (1) {
-
-      if(sw1_check()){
-          led_green_toggle();
-          while(sw1_check()){}
-      }
+    led_green_init();
+    led_red_init();
 
 
-    //delay();
-  }
+    while (1) {
+        //bucle esperando que se produzca la interrupcion
+    }
 
-  return 0;
+
+    return 0;
 }
