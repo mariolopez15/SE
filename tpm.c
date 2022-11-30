889c12fccec0d52ba4e8b50ec16cae3dbfb23bbf
mariolopez15 <mario.lopez@udc.es>
2022-11-30 13:22:58 +0100

    TPM implementado

--- tpm.c
+++ tpm.c
@@ -1,6 +1,7 @@
 #include "MKL46Z4.h"
 #include "lcd.h"
 #include <stdbool.h>
+#include "functions.h"
 
 // LED (RG)
 // LED_GREEN = PTD5 (pin 98)
@@ -20,131 +21,6 @@ uint8_t min;
 uint8_t seg;
 
 
-void irclk_ini()
-{
-  MCG->C1 = MCG_C1_IRCLKEN(1) | MCG_C1_IREFSTEN(1);
-  MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
-}
-
-void delay(void)
-{
-  volatile int i;
-
-  for (i = 0; i < 500000; i++);
-}
-
-// RIGHT_SWITCH (SW1) = PTC3
-void sw1_ini()
-{
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_IRQC(0xA);
-  GPIOC->PDDR &= ~(1 << 3);
-  NVIC_SetPriority(31, 0); //prioridad max
-  NVIC_EnableIRQ(31); //habilitamos la interrupcion
-}
-
-// LEFT_SWITCH (SW2) = PTC12
-void sw2_ini()
-{
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[12] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_IRQC(0xA);
-  GPIOC->PDDR &= ~(1 << 12);
-  NVIC_SetPriority(31, 0); //prioridad max
-  NVIC_EnableIRQ(31); //habilitamos la interrupcion
-}
-
-int sw1_check()
-{
-  return( !(GPIOC->PDIR & (1 << 3)) );
-}
-
-int sw2_check()
-{
-  return( !(GPIOC->PDIR & (1 << 12)) );
-}
-
-// RIGHT_SWITCH (SW1) = PTC3
-// LEFT_SWITCH (SW2) = PTC12
-void sws_ini()
-{
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
-  PORTC->PCR[3] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_IRQC(0xA) ;
-  PORTC->PCR[12] |= PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_IRQC(0xA) ;
-  GPIOC->PDDR &= ~(1 << 3 | 1 << 12);
-
-  NVIC_SetPriority(31, 0); //prioridad max
-  NVIC_EnableIRQ(31); //habilitamos la interrupcion
-}
-
-// LED_GREEN = PTD5
-void led_green_ini()
-{
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
-  PORTD->PCR[5] = PORT_PCR_MUX(1);
-  GPIOD->PDDR |= (1 << 5);
-  GPIOD->PSOR = (1 << 5);
-}
-
-void led_green_toggle()
-{
-  GPIOD->PTOR = (1 << 5);
-}
-
-void led_green_clear()
-{
-    GPIOD->PSOR = (1 << 5);
-}
-
-void led_green_set()
-{
-    GPIOD->PCOR = (1 << 5);
-}
-
-
-// LED_RED = PTE29
-void led_red_ini()
-{
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-  PORTE->PCR[29] = PORT_PCR_MUX(1);
-  GPIOE->PDDR |= (1 << 29);
-  GPIOE->PSOR = (1 << 29);
-}
-
-void led_red_toggle(void)
-{
-  GPIOE->PTOR = (1 << 29);
-}
-void led_red_clear(void)
-{
-    GPIOE->PSOR = (1 << 29);
-}
-
-void led_red_set(void)
-{
-    GPIOE->PCOR = (1 << 29);
-}
-
-
-// LED_RED = PTE29
-// LED_GREEN = PTD5
-void leds_ini()
-{
-  SIM->COPC = 0;
-  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK | SIM_SCGC5_PORTE_MASK;
-  PORTD->PCR[5] = PORT_PCR_MUX(1);
-  PORTE->PCR[29] = PORT_PCR_MUX(1);
-  GPIOD->PDDR |= (1 << 5);
-  GPIOE->PDDR |= (1 << 29);
-  // both LEDS off after init
-  GPIOD->PSOR = (1 << 5);
-  GPIOE->PSOR = (1 << 29);
-}
-
 
 void tpm_ini(){
 
@@ -154,7 +30,7 @@ void tpm_ini(){
     TPM0->SC = 0; //deshabilitamos el tpm para modificar el PS
     TPM0->SC = (TPM_SC_PS(4)); //dividimos por 32
     //Como trabaja a 32Khz pero lo dividimos entre 32 pasara 1s al completar 1000 ciclos
-    TPM0->MOD = TPM_MOD_MOD(2000); //El objetivo son 1000
+    TPM0->MOD = TPM_MOD_MOD(2000); //El objetivo son 2000
     TPM0->SC |= TPM_SC_TOIE_MASK; //habilitamos que se produzca la interrupcion
     NVIC_SetPriority(17, 0); //TPM0_IRQn es el 17
     NVIC_EnableIRQ(17);
@@ -165,7 +41,7 @@ void tmp_start(){
 }
 
 
-void minus_time(){
+void minus_time_tpm(){
 
     if(seg==0){
         if(min!=0){
@@ -185,7 +61,7 @@ void minus_time(){
 void FTM0IntHandler() {
     NVIC_ClearPendingIRQ(17);
     TPM0->SC |= TPM_SC_TOF_MASK; //al ser llamado la interrupcion pone el bit del overflow a 1 ya que el valor d ela cuenta alcanzo el objetivo
-    minus_time();
+    minus_time_tpm();
     lcd_display_time(min, seg);
 
 }
@@ -251,8 +127,6 @@ int main(void)
 {
   irclk_ini(); // Enable internal ref clk to use by LCD
 
-  led_green_ini();
-  led_red_ini();
   sws_ini();
   lcd_ini();
   tpm_ini();
